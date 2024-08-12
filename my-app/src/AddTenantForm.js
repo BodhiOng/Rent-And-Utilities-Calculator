5faae2856709345c187a6238994ccfefa93e6910
Bodhidharma Ong <bodhidharma202@gmail.com>
2024-08-12 20:44:01 +0700

    Finished add new tenant form card

--- my-app/src/AddTenantForm.js
+++ my-app/src/AddTenantForm.js
@@ -3,18 +3,21 @@ import "./AddTenantForm.css";
 function AddTenantForm() {
     return(
         <form>
+            <div className='title'>
+                <p className='title-text'>Add new tenant</p>
+            </div>
             <div className='form-group'>
                 <label htmlFor='tenantName'>Name</label>
-                <input type='text' className='form-control' id='tenantName' placeholder='Input your name here...'/>
+                <input type='text' className='form-control' id='tenantName' placeholder='Input your name here'/>
             </div>
             <div className='form-group'>
                 <label htmlFor='roomType'>Room Type</label>
                 <select className='form-control' id='roomType'>
-                    <option>Small</option>
-                    <option>Medium</option>
-                    <option>Master</option>
+                    <option value='' disable selected>Select your room type</option>
+                    <option value='small'>Small</option>
+                    <option value='medium'>Medium</option>
+                    <option value='master'>Master</option>
                 </select>
-
             </div>
         </form>
     );
