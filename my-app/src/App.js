ffc9ae5a2d1c8c83469da0a6a46474b9bee7620b
Bodhidharma Ong <bodhidharma202@gmail.com>
2024-08-12 17:16:56 +0700

    Set up React and Header in it

---
+++ my-app/src/App.js
@@ -0,0 +1,14 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './App.css';
+import Header from './Header';
+import './Header.css';
+
+function App() {
+  return (
+    <div className = 'header-div'>
+      <Header/>
+    </div>
+  );
+}
+
+export default App;
---
+++ my-app/src/Header.js
@@ -0,0 +1,13 @@
+import './Header.css';
+
+function Header() {
+    return (
+        <nav className="navbar navbar-dark bg-dark custom-navbar">
+            <p>
+                Rent & Utilities Calculator
+            </p>
+        </nav>
+    );
+}
+
+export default Header;
